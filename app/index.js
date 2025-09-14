fa3f1e9b9bae2594d6f4969bf6813bdffc8af84e
sqautboy <migonyoung01@gmail.com>
2025-09-14 18:19:46 +0900

    test: ci.cd test #1

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const version = 'v1.0.0';
+const version = 'v2.0.0';
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
